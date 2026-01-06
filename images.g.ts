0bfd9767e23d5a18a087e5e83010dfb96824690d
Gustav X <sillygorilla42@gmail.com>
2026-01-06 18:50:02 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "mario":return img`
+. . . . . . . . . . . . . . 
+. . . . 2 2 2 2 2 . . . . . 
+. . . 2 2 2 2 2 2 2 2 2 . . 
+. . . e e e d d e d . . . . 
+. . e d e d d d e d d d . . 
+. . e d e e d d d e d d d . 
+. . e e d d d d e e e e . . 
+. . . . d d d d d d d . . . 
+. . . e e 2 e e e . . . . . 
+. . e . . 2 e e 2 e e e . . 
+. e . . . 2 2 2 2 . . . . . 
+. . . . . 5 . . 5 . . . . . 
+. . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
