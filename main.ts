a7db288563c5bc27c58173fc34329ba90ce8d594
Gustav X <sillygorilla42@gmail.com>
2026-01-09 06:56:28 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mario.vy = -300
+    mario.vy = 150
 })
 function make_goombas () {
     for (let value of tiles.getTilesByType(assets.tile`myTile2`)) {
